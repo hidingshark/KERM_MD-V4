131894cbe8d8a1aef0c4241e170ff302bbe2a27f
hidingshark <VickyNMettle@outlook.com>
2024-12-13 19:52:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "2ExWECxL#PnBve86R0Ws0KYOF-vzVeioD_vz2P-uGfXA6jbOoLnk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
